e6292a15e4bcd7ee6c15a978c1a0fd81cba33754
Vladislav Yakovlev <vlad309523@gmail.com>
2023-07-26 03:57:58 +0400

    feat: add auto user management

---
+++ postgres-dbs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+function handle_container {
+  local container_id=$1
+  {
+    local envs=$(docker inspect -f '{{range .Config.Env}}{{println .}}{{end}}' "$container_id")
+    local db_url=$(echo "$envs" | grep DATABASE_URL | cut -d'=' -f2)
+
+    local protocol=$(echo $db_url | awk -F:// '{print $1}')
+
+    if [ "$protocol" != "postgresql" ]; then
+      return
+    fi
+
+    local creds=$(echo $db_url | awk -F[/:@] '{print $4, $5}')
+    local username=$(echo $creds | cut -d' ' -f1)
+    local password=$(echo $creds | cut -d' ' -f2)
+    local dbname=$(echo $db_url | awk -F[/] '{print $4}' | cut -d'?' -f1)
+
+    psql -c "CREATE ROLE $username WITH LOGIN PASSWORD '$password';"
+    psql -c "CREATE DATABASE $dbname WITH OWNER $PGUSER;"
+    psql -c "GRANT ALL PRIVILEGES ON DATABASE $dbname TO $username;"
+  } || {
+    echo "An error occurred while handling container $container_id"
+  }
+}
+
+# Handle all currently running containers
+for container_id in $(docker ps -q); do
+  handle_container $container_id
+done
+
+# Handle containers started in the future
+docker events --filter 'type=container' --filter 'event=start' | while read -r timestamp type action container_id rest; do
+  handle_container $container_id
+done
